f7d5a6bf0a785ee1653a2ed2342c88bc11b6b197
cerpinconsafo <pskolsky74@gmail.com>
2022-05-14 19:42:49 -0400

    codewars daily push - 8kyu grasshopper summation

---
+++ 8kyu/grasshopperSummation.js
@@ -0,0 +1,39 @@
+
+// 8 kyu
+// Grasshopper - Summation
+// 80325591% of 11,16742,169 of 116,448danleavitt01 Issue Reported
+
+//     JavaScript
+//     Node v8.1.3
+
+//         VIM
+//         EMACS
+
+// Instructions
+// Output
+// Summation
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+// cerpinconsafo
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/solutions/javascript/all/best_practices
+
+
+//plan of attack is simple for loop
+var summation = function (num) {
+    let sum = 0;
+    for (i = 1; i <= num; i++){
+      
+      sum += i;    
+      
+    }
+    return sum}
