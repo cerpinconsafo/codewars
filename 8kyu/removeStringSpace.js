d41ec224cb8a32b6b514affd391ef409c1b8aa81
cerpinconsafo <pskolsky74@gmail.com>
2022-05-16 13:46:40 -0400

    codewars daily push - 8kyu remove string space

---
+++ 8kyu/removeStringSpace.js
@@ -0,0 +1,26 @@
+
+// 8 kyu
+// Remove String Spaces
+// 71226290% of 11,14448,950 of 115,491PG1
+// JavaScript
+// Train AgainNext Kata
+
+//     Details
+//     Solutions
+//     Forks (7)
+//     Discourse (187)
+//     Translations
+
+// Fork|Collect|
+
+// Simple, remove the spaces from the string, then return the resultant string.
+// Fundamentals
+// Strings
+// Arrays
+
+
+// cerpinconsafo
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript/all/best_practices
+
+
+let noSpace = string => string.replace(/\s/g, "")
