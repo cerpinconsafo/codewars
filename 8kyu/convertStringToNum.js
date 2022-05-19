cef4df80b70f3c1cf96c998e151e87562d5b6c0b
cerpinconsafo <pskolsky74@gmail.com>
2022-05-19 19:06:43 -0400

    codewars daily push - 8kyu convert string to num

---
+++ 8kyu/convertStringToNum.js
@@ -0,0 +1,32 @@
+
+// 8 kyu
+// Convert a String to a Number!
+// 40412988% of 7,59638,164 of 94,063bkaes1 Issue Reported
+
+//     JavaScript
+//     Node v10.x
+
+//         VIM
+//         EMACS
+
+// Instructions
+// Output
+
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+// Description
+
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+// Examples
+
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+
+// cerpinconsafo
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/solutions/javascript
+
+const stringToNumber = str => parseInt(str)
