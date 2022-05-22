1308f6a124db5646e79af5641f9c239ab506502a
cerpinconsafo <pskolsky74@gmail.com>
2022-05-22 19:06:34 -0400

    codewars daily push - 8kyu return string

---
+++ 8kyu/returnStrings.js
@@ -0,0 +1,20 @@
+// 8 kyu
+// Returning Strings
+// 27210588% of 6,96831,064 of 80,704Aweson1
+// JavaScript
+// Train AgainNext Kata
+
+//     Details
+//     Solutions
+//     Forks (4)
+//     Discourse (144)
+//     Translations
+
+// Fork|Collect|
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/javascript/all/best_practices
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+let greet = name => `Hello, ${name} how are you doing today?`
