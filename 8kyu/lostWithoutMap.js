01a608b81aa1e5840123edff3d0d5e9bfa330594
cerpinconsafo <pskolsky74@gmail.com>
2022-05-26 15:43:58 -0400

    codewars daily push - 8kyu lost without a map

---
+++ 8kyu/lostWithoutMap.js
@@ -0,0 +1,16 @@
+// 8 kyu
+// Beginner - Lost Without a Map
+
+//     JavaScript
+
+
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/solutions/javascript/all/best_practices
+
+
+let maps = arr => arr.map( e => e*2 );
+
+//arrow function with only one argument being passed, an array arr
+
+//use map method on arr, where each element e of the array arr is doubled 
+
+//easy to ready and short code, can be codes out to be more 'human-readable'
