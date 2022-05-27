c3ccfd0f5c03dc2c647e5d59b2619ed8da449d1b
cerpinconsafo <pskolsky74@gmail.com>
2022-05-27 07:22:35 -0400

    codewars daily push - 8kyu cant code under pressure #1

---
+++ 8kyu/cantCodeUnderPressure1.js
@@ -0,0 +1,14 @@
+// 8 kyu
+// You Can't Code Under Pressure #1
+// =
+// JavaScript
+
+// Fundamentals
+// Numbers
+// Integers
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/solutions/javascript/all/best_practices
+
+let doubleInteger = (num) => num*2;
