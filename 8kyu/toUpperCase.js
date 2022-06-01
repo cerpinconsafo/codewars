2ef0a17d9649798bc4bcfbd7a80e7881a9a22d16
cerpinconsafo <pskolsky74@gmail.com>
2022-06-01 18:52:47 -0400

    codewars daily push - 8kyu to upper case

---
+++ 8kyu/toUpperCase.js
@@ -0,0 +1,25 @@
+// 8 kyu
+// MakeUpperCase
+// 2443785% of 6,19922,811 of 60,373wichu
+// JavaScript
+// Train AgainNext Kata
+
+//     Details
+//     Solutions
+//     Forks (7)
+//     Discourse (75)
+//     Translations
+
+// Fork|Collect|
+
+// Write a function which converts the input string to uppercase.
+
+
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+  }
+
+or 
+
+const makeUpperCase = str => { str.toUpperCase() } 
