bfded77c6bc142b1471c908c5f5e2f36e7b1e686
cerpinconsafo <pskolsky74@gmail.com>
2022-06-02 16:49:21 -0400

    codewars daily push - 8kyu multiply two num

---
+++ 8kyu/multiplyTwoNums.js
@@ -0,0 +1,31 @@
+// 8 kyu
+// Function 3 - multiplying two numbers
+// 1424581% of 4,20616,415 of 44,616ineiti
+// JavaScript
+// Train AgainNext Kata
+
+//     Details
+//     Solutions
+//     Forks (3)
+//     Discourse (89)
+//     Translations
+
+// Fork|Collect|
+
+// Implement a function which multiplies two numbers.
+
+
+// super simple but the test was annoying/unspecific about function naming
+
+
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/solutions/javascript/me/best_practices
+
+// Write here your multiply-function
+function multiply(n1,n2) {
+    return n1*n2; 
+   }
+
+
+//    es6
+
+   const multiply = (n1, n2) => n1 * n2;
