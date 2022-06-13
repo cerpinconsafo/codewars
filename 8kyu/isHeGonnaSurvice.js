8c386324cb8266682f21452c1e71ee22b61c7710
cerpinconsafo <pskolsky74@gmail.com>
2022-06-12 22:53:57 -0400

    codewars daily push - 8kyu is he gonna survive

---
+++ 8kyu/isHeGonnaSurvice.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/solutions/javascript/me/best_practice
+
+function hero(bullets, dragons){
+    if ((dragons * 2) <= bullets) return true
+    else {return false}
+    //Get Coding!
+  }
