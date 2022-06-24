de25d2fbd634cd0a36660e9c90b3d9ef0407c0c4
cerpinconsafo <pskolsky74@gmail.com>
2022-06-24 10:48:54 -0400

    codewars daily push 8kyu basic variable assignment

---
+++ 8kyu/basicvariableAssignment.js
@@ -0,0 +1,18 @@
+// 8 kyu
+// Basic variable assignment
+// 110978% of 3,63612,792 of 33,085Ira
+// JavaScript
+
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+// should be 
+
+
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
