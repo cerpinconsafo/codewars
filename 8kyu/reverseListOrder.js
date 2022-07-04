806b953154d9c2190fe1dde4acf123519cd9c597
cerpinconsafo <pskolsky74@gmail.com>
2022-07-04 11:35:47 -0400

    codewars daily push - 8kyu reverse list order

---
+++ 8kyu/reverseListOrder.js
@@ -0,0 +1,25 @@
+// 8 kyu
+// Reverse List Order
+// 974090% of 2,5258,578 of 21,758cmgerber
+// JavaScript
+
+// Description:
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// Examples (Input -> Output)
+
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+// Lists
+// Data Structures
+// Fundamentals
+
+//https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/javascript
+
+// long form
+// // function reverseList(list) {
+//   return list.reverse();
+// }
+
+let reverseList = (list) => list.reverse()
