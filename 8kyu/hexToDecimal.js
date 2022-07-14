fcd1c02897bc973da55392ceec39321bf381900c
cerpinconsafo <pskolsky74@gmail.com>
2022-07-13 20:40:55 -0400

    codewars daily push - 8kyu hex to deci and n divis

---
+++ 8kyu/hexToDecimal.js
@@ -0,0 +1,18 @@
+// 8 kyu
+// Hex to Decimal
+// 2214589% of 2,4406,131 of 16,954wichu
+// JavaScript
+
+// Description:
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+// Fundamentals
+
+// https://www.codewars.com/kata/57a4d500e298a7952100035d/javascript
+
+// my solution:
+
+function hexToDec(hexString){
+    //take in the variable hextring and 16 for hexidemical radix
+    return parseInt(hexString, 16);
+  }
---
+++ 8kyu/isNdivisibleByXandY.js
@@ -0,0 +1,30 @@
+// 8 kyu
+// Is n divisible by x and y?
+// 35913191% of 8,35126,712 of 72,753naaz
+// JavaScript
+
+
+// Description:
+
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+// Examples:
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+
+// Refactoring
+
+
+// https://www.codewars.com/kata/5545f109004975ea66000086/javascript
+
+// my solution:
+
+let isDivisible = (n, x, y) => (n%x === 0) & (n%y === 0) ? true : false;
+
+// top best:
+
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0
+  }
