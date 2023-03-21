e767518481a97f6ed537914172ce8acb77bf1da0
cerpinconsafo <pskolsky74@gmail.com>
2023-03-21 12:25:39 -0400

    added to last solution

--- 8kyu/convertBooltoStr.js
+++ 8kyu/convertBooltoStr.js
@@ -7,6 +7,17 @@
 // Fundamentals
 // https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
 
-// ES6 nice version
+// ES6 nice version, my solution
 let boolToWord = bool =>  (bool == true)? "Yes" : "No"
-//  if bool is truthy, return string 'Yes', otherwise return string 'No'
+    //  if bool is truthy, return string 'Yes', otherwise return string 'No'
+
+
+
+// wordy readable solution
+function boolToWord( bool ){
+    if (bool) {
+      return 'Yes';
+    } else {
+      return 'No';
+    }
+  }
